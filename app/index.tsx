eab2439d241aee3c4bc0adad3a01306aaf344227
pho-veteran <128946325+pho-veteran@users.noreply.github.com>
2025-04-01 20:52:05 +0700

    Initial | GlueStackUI, NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Box } from "@/components/ui/box";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box className="bg-primary-500 p-5">
+        <Text className="text-typography-0">This is the Box</Text>
+      </Box>
+    </View>
+  );
+}
